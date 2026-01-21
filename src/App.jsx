c577af0d27cdc6500b990bccaa0b8db07dd36381
Sonu Kumar <krsonu025@gmail.com>
2026-01-21 12:28:48 +0530

    Get input field value

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import User from "../USer";
+import { useState } from "react";
 
 function App(){
+  const[val,setValue]=useState("");
   return (
+
     <>
-    <h1>props in react js</h1>
-    <User name="sonu Kumar"/>
+    <h2>Get Input field Value</h2>
+    <input type="text" value={val} onChange={(event)=>setValue(event.target.value)}placeholder="Enter Name"/>
+    <h2>{val}</h2>
+    <button onClick={()=>setValue("")}>clear</button>
     </>
   )
 }
+
 export default App;
