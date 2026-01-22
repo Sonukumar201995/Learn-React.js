3dc6c97a18aa256443b595ad450b9d6e9d98833f
Sonu Kumar <krsonu025@gmail.com>
2026-01-22 13:09:36 +0530

    Nested Looping

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import Clock from "./clock";
-function App()
-{
-  return(
-      <>
-    <h1>Digital Clock in React js </h1>
-    <Clock/>
-    </>
-  )
+function App() {
+  const students = [
+    {
+      name: "Sonu",
+      subjects: ["Math", "Java", "React"],
+    },
+    {
+      name: "Monu",
+      subjects: ["C", "Python", "Node"],
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Nested Looping in React</h1>
+
+      {students.map((student, index) => (
+        <div key={index}>
+          <h2>{student.name}</h2>
+
+          <ul>
+            {student.subjects.map((sub, i) => (
+              <li key={i}>{sub}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
+
 export default App;
