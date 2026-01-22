3ed7f88bddaba160589f5fe45a09f86555e94c94
Sonu Kumar <krsonu025@gmail.com>
2026-01-22 13:04:35 +0530

    clock with clear

--- src/clock.jsx
+++ src/clock.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 
 function Clock() {
-  const [time, setTime] = useState(0);
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
 
   useEffect(() => {
-    setInterval(() => {
+    const interval = setInterval(() => {
       setTime(new Date().toLocaleTimeString());
     }, 1000);
+
+    return () => clearInterval(interval); // cleanup
   }, []);
 
   return (
