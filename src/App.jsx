0883fd1c328216a66bf56ce3ac6999b4f236b49b
Sonu Kumar <krsonu025@gmail.com>
2026-01-24 15:12:23 +0530

    inline css in react js

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-import { useEffect, useState } from "react";
-
-function App()
-{
-
-  const [counter,setCounter]=useState(0);
-
-  function callOnce(){
-    console.log("call once function called");
-  }
-
-  useEffect(()=>{
-    callOnce();
-  })
-  // callOnce();
+function App(){
   return(
 
     <div>
-      <h1>useEffect Hooks</h1>
-      <button onClick={()=>setCounter(counter+1)}>Button</button>
+      <h2 style={{color:'red',backgroundColor:'green'}}>Inline css in React js</h2>
     </div>
   )
 }
