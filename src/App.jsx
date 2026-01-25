63d7039a473fa62ec0b7f98b67c12bb55c769969
Sonu Kumar <krsonu025@gmail.com>
2026-01-25 17:00:46 +0530

    useRef and property

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,18 @@ function App()
  const handleRef=()=>{
   console.log(inputRef);
   inputRef.current.focus();
+  inputRef.current.style.color = "red";
+inputRef.current.style.backgroundColor = "yellow";
+inputRef.current.style.fontSize = "20px";
+inputRef.current.style.border = "2px solid black";
+inputRef.current.style.padding = "10px";
+
  }
   return(
     <>
     <h1>useRef in React js</h1>
     <input ref={inputRef} type="text" placeholder="Enter Name" />
-    <button onClick={handleRef}>focus</button>
+    <button onClick={handleRef}>click</button>
     </>
   )
 }
