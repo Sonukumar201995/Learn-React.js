3c682b00ad7303b7a52dd5e310feb8bbe60e5dae
Sonu Kumar <krsonu025@gmail.com>
2026-01-26 15:00:44 +0530

    uncontrolled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import { useRef } from "react";
+function App() {
 
-function App()
-{
+  const handleForm = (event) => {
+    event.preventDefault();
 
- let inputRef=useRef(null);
- const handleRef=()=>{
-  console.log(inputRef);
-  inputRef.current.focus();
-  inputRef.current.style.color = "red";
-inputRef.current.style.backgroundColor = "yellow";
-inputRef.current.style.fontSize = "20px";
-inputRef.current.style.border = "2px solid black";
-inputRef.current.style.padding = "10px";
+    const user = document.querySelector('#user').value;
+    const pass = document.querySelector('#password').value;
 
- }
-  return(
+    console.log("Username:", user);
+    console.log("Password:", pass);
+  };
+
+  return (
     <>
-    <h1>useRef in React js</h1>
-    <input ref={inputRef} type="text" placeholder="Enter Name" />
-    <button onClick={handleRef}>click</button>
+      <h2>Uncontrolled Components</h2>
+
+      <form onSubmit={handleForm}>
+        <input type="text" id="user" placeholder="Enter Name" />
+        <br /><br />
+
+        <input type="password" id="password" placeholder="Enter Password" />
+        <br /><br />
+
+        <button type="submit">Submit</button>
+       
+
+      </form>
     </>
-  )
+  );
 }
+
 export default App;
