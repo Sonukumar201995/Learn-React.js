b5fa1582b26f60a4a8da125a4c51eb3955767fcf
Sonu Kumar <krsonu025@gmail.com>
2026-01-27 13:00:33 +0530

    useFormStatus React js

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-import { useRef } from "react";
-import InputBox from "./InputBox";
+import { useFormStatus } from "react-dom";
 
 function App() {
-  const inputRef = useRef();
+  const handleSubmit = async () => {
+    await new Promise((res) => setTimeout(res, 2000));
+    console.log("submitted");
+  };
+
+  function CustomerForm() {
+    const { pending } = useFormStatus();
+
+    return (
+      <>
+        <input type="text" placeholder="Enter Name" />
+        <br /><br />
+        <input type="password" placeholder="Enter Password" />
+        <br /><br />
+        <button disabled={pending}>
+          {pending ? "Submitting..." : "Submit"}
+        </button>
+      </>
+    );
+  }
 
   return (
-    <>
-      <h2>forwardRef Example</h2>
-      <InputBox ref={inputRef} />
+    <div>
+      <h2>useFormStatus React</h2>
 
-      <button onClick={() => inputRef.current.focus()}>
-        Focus Input
-      </button>
-    </>
+      <form action={handleSubmit}>
+        <CustomerForm />
+      </form>
+    </div>
   );
 }
 
--- src/inputBox.jsx
+++
@@ -1,7 +0,0 @@
-import { forwardRef } from "react";
-
-const InputBox = forwardRef((props, ref) => {
-  return <input type="text" ref={ref} placeholder="Type here..." />;
-});
-
-export default InputBox;
