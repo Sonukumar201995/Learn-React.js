77357b382ea50b82790aa4194d28d2db33e85141
Sonu Kumar <krsonu025@gmail.com>
2026-02-02 16:36:39 +0530

    update objects in state

--- User.jsx
+++
@@ -1,8 +0,0 @@
-function User({setUser}){
-    return(
-        <div>
-            <input onChange={(event)=>setUser(event.target.value)}type="text" placeholder="Enter Name"/>
-        </div>
-    )
-}
-export default User;
--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,71 @@
-
-
-import User from "../User";
-import Display from "../Display";
 import { useState } from "react";
-function App(){
 
-  const[user,setUser]=useState('')
-  return(
+function App() {
+  const [data, setData] = useState({
+    name: "sonu",
+    address: {
+      city: "Delhi",
+      country: "India"
+    }
+  });
+
+  const handleName = (val) => {
+    setData(prev => ({
+      ...prev,
+      name: val
+    }));
+  };
+
+  const handleCity = (val) => {
+    setData(prev => ({
+      ...prev,
+      address: {
+        ...prev.address,
+        city: val
+      }
+    }));
+  };
+
+  const handleCountry = (val) => {
+    setData(prev => ({
+      ...prev,
+      address: {
+        ...prev.address,
+        country: val
+      }
+    }));
+  };
+
+  return (
     <div>
-      <h2>Lifting state in React</h2>
-      <User setUser={setUser}/>
-      <hr />
-      <Display user={user}/>
+      <h1>Updating Objects in State</h1>
+
+      <input
+        type="text"
+        placeholder="Enter Name"
+        value={data.name}
+        onChange={(e) => handleName(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Enter City"
+        value={data.address.city}
+        onChange={(e) => handleCity(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Enter Country"
+        value={data.address.country}
+        onChange={(e) => handleCountry(e.target.value)}
+      />
+
+      <h2>Name: {data.name}</h2>
+      <h2>City: {data.address.city}</h2>
+      <h2>Country: {data.address.country}</h2>
     </div>
-    
-  )
+  );
 }
 
 export default App;
