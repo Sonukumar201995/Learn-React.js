f0f0e3a2c2898b5866d04bb70e712eee476b3a53
Sonu Kumar <krsonu025@gmail.com>
2026-02-04 15:44:33 +0530

    useActiontate in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
-import { useId } from "react";
+import { useActionState } from "react";
 
 function App() {
-  const nameId = useId();
+
+  const handleSubmit = (previousData, formData) => {
+    let name = formData.get('name');
+    let password = formData.get('password');
+
+    console.log("handleSubmit called", name, password);
+
+    return { name, password }; // new state return karna hota hai
+  }
+
+  const [data, action, pending] = useActionState(handleSubmit, null);
 
   return (
     <div>
-      <label htmlFor={nameId}>Name:</label>
-      <input id={nameId} type="text" />
-      <p>{nameId}</p>
+      <h2>useActionState in React</h2>
+
+      <form action={action}>
+        <input type="text" placeholder="Enter Name" name="name" />
+        <br /><br />
+        <input type="text" placeholder="Enter Password" name="password" />
+        <br /><br />
+        <button disabled={pending}>Submit Data</button>
+      </form>
+
+      {data && (
+        <h3>
+          Name: {data.name} | Password: {data.password}
+        </h3>
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
