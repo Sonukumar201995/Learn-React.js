b1c41e085f8cda97fe929e97ee3f27a0e690e574
Sonu Kumar <krsonu025@gmail.com>
2026-02-04 15:32:10 +0530

    useId hook in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-import { useState } from "react";
+import { useId } from "react";
 
 function App() {
-  const [names, setNames] = useState(["Sonu", "Amit"]);
-
-  const addName = () => {
-    setNames([...names, "Rahul"]);
-  };
+  const nameId = useId();
 
   return (
     <div>
-      <h1>Updating array in React</h1>
-
-      {names.map((item, index) => (
-        <h2 key={index}>{item}</h2>
-      ))}
-
-      <button onClick={addName}>Add Name</button>
+      <label htmlFor={nameId}>Name:</label>
+      <input id={nameId} type="text" />
+      <p>{nameId}</p>
     </div>
   );
 }
