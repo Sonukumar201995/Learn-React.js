f6782b623a6b798ad7d19bc3b5768a3884b8110f
Sonu Kumar <krsonu025@gmail.com>
2026-02-09 15:42:10 +0530

    404 page and redirection

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Contact from "./Contact";
 import Login from "./Login";
 import NavBar from "./assets/NavBar";
+import NotFound from "./NotFound";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar/>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
