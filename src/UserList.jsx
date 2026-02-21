84ff56f47e8b58940a6af8a6d8f8d14df068615a
Sonu Kumar <krsonu025@gmail.com>
2026-02-21 14:58:22 +0530

    Added UserList component with delete functionality

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -1,32 +1,40 @@
 import { useEffect, useState } from "react";
 
-export default function UserLsit() {
+export default function UserList() {
 
+  const [userData, setUserData] = useState([]);
 
-  const [userData,setUserData]=useState([]);
   useEffect(() => {
     getUserData();
-  }, []);  
+  }, []);
 
   const getUserData = async () => {
-    const url = "http://localhost:3000/users";
+    const response = await fetch("http://localhost:3000/users");
+    const data = await response.json();
+    setUserData(data);
+  };
 
-    let response = await fetch(url);
-    response=await response.json();  
+  const deleteUser = async (id) => {
+    const response = await fetch(`http://localhost:3000/users/${id}`, {
+      method: "DELETE",
+    });
 
-    console.log(response);
-    setUserData(response);
+    if (response.ok) {
+      alert("Record deleted");
+      getUserData(); // reload list
+    }
   };
 
   return (
     <div>
-          {
+      {
         userData.map((user) => (
           <div key={user.id}>
             <h3>Name: {user.name}</h3>
             <h3>Age: {user.age}</h3>
             <h3>Email: {user.email}</h3>
             <hr />
+            <button onClick={() => deleteUser(user.id)}>Delete</button>
           </div>
         ))
       }
